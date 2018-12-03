df4ae9fb76d2c956b7e00a71b54d8bf0337ed400
a-bezrukov <36845392+a-bezrukov@users.noreply.github.com>
2018-12-03 18:10:10 +0400

    Refactor mtp stripped (#10)
    * MTP data was stripped off
    * Added a simple startup script
    * Minor fix to the startup script

---
+++ scripts/start_electrumx.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+export COIN=zcoin 
+export DAEMON_URL=http://YANNISCOOLUSER:YANNPASSISCOOL@localhost:18888/ 
+export DB_DIRECTORY=/home/electrumx/db/
+export NET=testnet
+export SSL_CERTFILE=/home/electrumx/server.crt
+export SSL_KEYFILE=/home/electrumx/server.key
+export HOST=0.0.0.0
+export SSL_PORT=51002
+export TCP_PORT=51001
+
+WD="$( cd "$(dirname "$0")" ; pwd -P )"
+
+python3.6 $WD/../electrumx_server
